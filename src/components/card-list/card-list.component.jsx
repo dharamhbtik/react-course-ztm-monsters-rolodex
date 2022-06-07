011eb4a12d27f7938781adf334d5cc8c5e4314d5
Dharmendra Kumar <dharmendra.hbti@gmail.com>
2022-06-07 19:39:30 +0530

    Adding to repo

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+import './card-list.styles.css';
+import Card from './card/card.component';
+
+class CardList extends Component{
+
+    render(){
+        const {monsters} = this.props;
+        return (<div  className='card-list'>
+              
+                {monsters.map((m)=>{
+                   
+                    return (
+                        <Card key={m.id} monster={m} />
+                    )
+                }
+                )}
+                </div>);
+            
+            
+    }
+}
+export default CardList;
